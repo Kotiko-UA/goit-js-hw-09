b687562804e254814604ecbb95a245f40c3e5ec9
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-07-07 14:48:13 +0300

    task 1 complite

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  btStart: document.querySelector('button[data-start]'),
+  btStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+refs.btStart.addEventListener('click', onStartClick);
+refs.btStop.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  refs.btStart.disabled = true;
+
+  colorChangeInteval = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function onStopClick() {
+  refs.btStart.disabled = false;
+  clearInterval(colorChangeInteval);
+}
