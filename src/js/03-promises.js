8e997e247e3e665142b9276845037f8efb39bf10
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-07-07 22:43:09 +0300

    complite task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const firstDelay = e.currentTarget.elements.delay.value;
+  const step = e.currentTarget.elements.step.value;
+  const amount = e.currentTarget.elements.amount.value;
+  let currentDelay = Number(firstDelay);
+
+  for (let i = 0; i < amount; i += 1) {
+    let position = i + 1;
+    let delay = currentDelay; // Створюємо копію змінної delay
+
+    console.log(delay);
+
+    setTimeout(() => {
+      createPromise(position, delay)
+        .then(() => {})
+        .catch(() => {});
+    }, delay);
+
+    currentDelay += Number(step);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`));
+    } else {
+      reject(Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+    }
+  });
 }
